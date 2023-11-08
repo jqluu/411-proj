f2c27285a6964bb2d0c6bb75f083fc19b8b47ac7
Jonathan Luu <72171222+jqluu@users.noreply.github.com>
2023-11-08 10:27:15 -0800

    init

---
+++ ServiceExchange/ServiceExchange/ServiceExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceExchangeApp.swift
+//  ServiceExchange
+//
+//  Created by jluu on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ServiceExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
